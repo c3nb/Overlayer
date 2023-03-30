42c7404c1a90cfd849b89ffb5300d3e57394f306
Runas8128 <runas8128@naver.com>
2023-03-30 12:00:10 +0900

    feat: add CurHitTag

---
+++ Overlayer/Tags/CurHitTags.cs
@@ -0,0 +1,28 @@
+﻿using Overlayer.Core.Tags;
+using Overlayer.Patches;
+
+namespace Overlayer.Tags
+{
+    public class CurHitTags
+    {
+        private static GetHitMarginFixer.PerDiff Diff =>
+            GetHitMarginFixer.diff[GCS.difficulty];
+
+        [Tag("CurHit")]
+        public static string Hit() => RDString.Get("HitMargin." + Diff.NowMargin);
+        [Tag("CTE")]
+        public static double TE() => Diff.Counts[HitMargin.TooEarly];
+        [Tag("CVE")]
+        public static double VE() => Diff.Counts[HitMargin.VeryEarly];
+        [Tag("CEP")]
+        public static double EP() => Diff.Counts[HitMargin.EarlyPerfect];
+        [Tag("CP")]
+        public static double P() => Diff.Counts[HitMargin.Perfect];
+        [Tag("CLP")]
+        public static double LP() => Diff.Counts[HitMargin.LatePerfect];
+        [Tag("CVL")]
+        public static double VL() => Diff.Counts[HitMargin.VeryLate];
+        [Tag("CTL")]
+        public static double TL() => Diff.Counts[HitMargin.TooLate];
+    }
+}
