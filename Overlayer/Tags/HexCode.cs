48b64610bf9aa0b9a2a7d553249b29c19648f680
Runas8128 <runas8128@naver.com>
2023-04-09 01:33:20 +0900

    *Hex Tags?

---
+++ Overlayer/Tags/HexCode.cs
@@ -0,0 +1,28 @@
+﻿using Overlayer.Core.Tags;
+
+namespace Overlayer.Tags
+{
+    public static class HexCode
+    {
+        [FieldTag("TEHex")]
+        public static string TEHex = "FF0000FF";
+        [FieldTag("VEHex")]
+        public static string VEHex = "FF6F4EFF";
+        [FieldTag("EPHex")]
+        public static string EPHex = "A0FF4EFF";
+        [FieldTag("PHex")]
+        public static string PHex = "60FF4EFF";
+        [FieldTag("LPHex")]
+        public static string LPHex = "A0FF4EFF";
+        [FieldTag("VLHex")]
+        public static string VLHex = "FF6F4EFF";
+        [FieldTag("TLHex")]
+        public static string TLHex = "FF0000FF";
+        [FieldTag("MPHex")]
+        public static string MPHex = "00FFEDFF";
+        [FieldTag("FMHex")]
+        public static string FMHex = "D958FFFF";
+        [FieldTag("FOHex")]
+        public static string FOHex = "D958FFFF";
+    }
+}
