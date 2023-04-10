90cf611b97a9d6e8d2a4fe42f7b7ad3bef4a4322
C#Newbie <dongsu45681111@gmail.com>
2023-04-11 00:00:40 +0900

    ｌｏｇｏ

--- Overlayer/Patches/RemoveFuckingUMMDummyLogPatch.cs
+++ Overlayer/Patches/RemoveFuckingUMMDummyLogPatch.cs
@@ -9,15 +9,4 @@ namespace Overlayer.Patches
     {
         public static bool Prefix(string str) => str != "Cancel start. Already started.";
     }
-    [HarmonyPatch(typeof(scrController), "Hit")]
-    public static class TestPatch
-    {
-        [FieldTag("HitCount", NotPlaying = true, RelatedPatches = "Overlayer.Patches.TestPatch:Postfix")]
-        public static int Count = 0;
-        public static void Postfix()
-        {
-            Count++;
-            Main.Logger.Log("Count!!");
-        }
-    }
 }
